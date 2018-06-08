97ae460d9f88d1e542e9f4436873e41fd210a8a4
Parker Reed <parker.l.reed@gmail.com>
2018-06-07 20:04:09 -0400

    Implement basic scanner. Only base frequencies for now.

--- scripts/README.md
+++ scripts/README.md
@@ -11,3 +11,13 @@ Script that accepts an RF channel number along with subchannel and plays it in a
 You can get your local channel mappings from https://www.tablotv.com/tools/
 
 py3 version for Python 3. In Python 2 strings sent over a socket were already byte streams. Python 3 requires the data to be explicitly formatted as a byte stream.
+
+## tv-scanner.py
+
+Credit: parkerlreed
+
+Script to scan for the base frequencies you can receive. Prints as it's scanning and then once more at the end.
+
+TODO: Implement mpeg2 library to verify valid signal and grab program/subchannel data.
+
+Python 3 required. Python 2 needs modification to the message strings to work.
---
+++ scripts/tv-scanner.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import socket
+
+TCP_IP = '192.168.1.1'
+TCP_PORT = 6000
+BUFFER_SIZE = 1024
+CHOICES = [0, 57, 63, 69, 79, 85, 177, 183, 189, 195, 201, 207, 213, 473, 479, 485, 491, 497, 503, 509, 515, 521, 527, 533, 539, 545, 551, 557, 563, 569, 575, 581, 587, 593, 599, 605, 611, 617, 623, 629, 635, 641, 647, 653, 659, 665, 671, 677, 683, 689, 695, 701, 707, 713, 719, 725, 731, 737, 743, 749, 755, 761, 767, 773, 779, 785, 791, 797, 803]
+MESSAGE1 = '<msg type="login_req"><params protocol="TCP" port="8000"/></msg>'
+CHANLIST = []
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+s.send(bytes(MESSAGE1, encoding='utf-8'))
+data = s.recv(BUFFER_SIZE)
+print(data)
+s.close()
+
+
+for i in CHOICES:
+    try:
+        MESSAGE2 = '<msg type="tune_req"><params tv_type="ATSC" freq="' + str(i) +'000" bandwidth="8000" plp="0" programNumber="0"/></msg>'
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(5)
+        s.connect((TCP_IP, TCP_PORT))
+        s.send(bytes(MESSAGE2, encoding='utf-8'))
+        data = s.recv(BUFFER_SIZE)
+        print(data)
+        if 'ret="0"' in str(data):
+            CHANLIST.append(str(i))
+            print(CHANLIST)
+        s.close()
+    except socket.timeout:
+        continue
+
+print(CHANLIST)
